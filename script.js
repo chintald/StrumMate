1f4060035e7897e8427e7b7d1fe5189d340611ee
Chintal Raval <chintaldraval@gmail.com>
2023-02-20 22:57:34 -0500

    Initial Commit - Sliders are working, can see the console logs for inputs.

---
+++ script.js
@@ -0,0 +1,31 @@
+// // Get references to the slider input elements
+// var $slider1 = $('#slider1 input[type="range"]');
+// var $slider2 = $('#slider2 input[type="range"]');
+
+
+// // Listen for changes to the slider values
+// $slider1.on('input', function() {
+//   var value = $(this).val();
+//   // Do something with the value of slider1
+//   console.log('Slider 1 value: ' + value);
+// });
+
+// $slider2.on('input', function() {
+//   var value = $(this).val();
+//   // Do something with the value of slider2
+//   console.log('Slider 2 value: ' + value);
+// });
+
+$(document).ready(function() {
+
+  $('#slider1').change(function() {
+    var value = $(this).val();
+    console.log(value);
+  });
+
+  $('#slider2').change(function() {
+    var value = $(this).val();
+    console.log(value);
+  });
+
+});
