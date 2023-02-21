8a88d365c74343fee76d02967736be1251a39a02
Chintal Raval <chintaldraval@gmail.com>
2023-02-21 12:51:58 -0500

    wip handle logic

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-// // Get references to the slider input elements
-// var $slider1 = $('#slider1 input[type="range"]');
-// var $slider2 = $('#slider2 input[type="range"]');
 
-
-// // Listen for changes to the slider values
-// $slider1.on('input', function() {
-//   var value = $(this).val();
-//   // Do something with the value of slider1
-//   console.log('Slider 1 value: ' + value);
-// });
-
-// $slider2.on('input', function() {
-//   var value = $(this).val();
-//   // Do something with the value of slider2
-//   console.log('Slider 2 value: ' + value);
-// });
+// var five = require('johnny-five');
+// var board = new five.Board();
 
 $(document).ready(function() {
 
@@ -29,3 +15,4 @@ $(document).ready(function() {
   });
 
 });
+
