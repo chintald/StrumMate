4860d3eb0544a807f094bb713ae2a69ddfb6774f
Chintal Raval <chintaldraval@gmail.com>
2023-02-23 22:45:10 -0500

    changed length of handle and starting angle for it(var baseAngle)

--- script.js
+++ script.js
@@ -21,9 +21,10 @@ $(document).ready(function () {
   });
 
   var line = document.getElementById("line");
-  line.setAttribute("transform", "rotate(" + 90 + ", 100, 100)");
+  line.setAttribute("transform", "rotate(" + 160 + ", 100, 100)");
 
-  var i = 90;
+  var i = 160;
+  var baseAngle = 160;
   var ltr = 1;
   var timer = null;
 
@@ -33,16 +34,16 @@ $(document).ready(function () {
       ltr === -1 && (i -= 1);
       line.setAttribute("transform", "rotate(" + i + ", 100, 100)");
       i === maxAngle && (ltr = -1);
-      i === 90 && (ltr = 1);
+      i === baseAngle && (ltr = 1);
     }, timeInterval);
   }
 
   const clearTimer = () => {
     timer && clearInterval(timer);
     timer = null;
-    i = 90
+    i = baseAngle
     ltr = 1;
-    line.setAttribute("transform", "rotate(" + 90 + ", 100, 100)");
+    line.setAttribute("transform", "rotate(" + baseAngle + ", 100, 100)");
   }
 
   var slider1Val = 0;
